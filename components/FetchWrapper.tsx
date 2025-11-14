ec770d68aecda8c8987068839a5e8f22b254a8af
zakky <fakhrimuzakki06@gmail.com>
2025-11-14 15:19:02 +0700

    coba

--- components/FetchWrapper.tsx
+++ components/FetchWrapper.tsx
@@ -3,8 +3,8 @@ import { Product } from "@/interfaces/interface";
 
 const FetchWrapper = async ({ children }: React.PropsWithChildren) => {
   const res = await fetch("https://fakestoreapi.com/products", {
-    // next: { revalidate: 2_592_000 },
-    cache: "no-store",
+    next: { revalidate: 2592000 },
+    // cache: "no-store",
     signal: AbortSignal.timeout(10000),
   });
 
