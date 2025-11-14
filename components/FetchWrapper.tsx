ad1e7b6cb9179b5de56c632636a62a3359466865
zakky <fakhrimuzakki06@gmail.com>
2025-11-14 14:57:33 +0700

    fig bux

--- components/FetchWrapper.tsx
+++ components/FetchWrapper.tsx
@@ -3,7 +3,8 @@ import { Product } from "@/interfaces/interface";
 
 const FetchWrapper = async ({ children }: React.PropsWithChildren) => {
   const res = await fetch("https://fakestoreapi.com/products", {
-    next: { revalidate: 2_592_000 },
+    // next: { revalidate: 2_592_000 },
+    cache: "no-store",
   });
 
   if (!res.ok) throw new Error("Failed to fetch users");
